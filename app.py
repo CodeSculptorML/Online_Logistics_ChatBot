6b5de6fa11913258295d9f36ef437b4146253ec5
CodeSculptorML <malhotrakiara928@gmail.com>
2024-01-29 11:15:04 +0530

    success

--- app.py
+++ app.py
@@ -7,84 +7,28 @@ import nltk
 from nltk.chat.util import Chat, reflections
 
 extended_price_negotiation_pairs = [
-    ["hi|hello|hey", ["Hello! How may I help you?"]],
-    ["show products", ["We have Bikes, accessories, fashion, etc."]],
-    ["show available products", ["We have Bikes, accessories, fashion, etc."]],
-    ["available products", ["We have Bikes, accessories, fashion, etc."]],
-    ["products", ["We have Bikes, accessories, fashion, etc."]],
-    ["bikes", ["We have touring bikes, road bikes, mountain bikes. See anything you like? Just ask it."]],
-    ["accessories", ["We have mobile phones, laptops, washing machine. See anything you like? Just ask it."]],
-    ["fashion", ["We have kids fashion, womens fashion, mens fashion. See anything you like? Just ask it."]],
+    ["hi|hello|hey", ["Hello I'm the Online Virtual Assistant.I'm here to help you with your questions regarding logistics services"]],
+    ["How are you?", ["I am doing well,How are you?"]],
+    ["I am fine.I have some doubts", ["Please ask me your questions about products and services."]],
+    ["when will my package be delivered?", ["can you please send me your tracking ID ? so that i can check the estimated time of delivery."]],
+    ["Sure,here is my tracking ID ABD34Y6J", ["Thanks for providing tracking ID.Let me check."]],
+    [" okay", ["Your package with tracking number ABD34Y6J is currently in transit and expected to be delivered in 3-4 days."]],
+    ["Can you help me schedule a pickup for a package?", ["Of course! Please provide the pickup address, package dimensions, weight, and preferred pickup date and time."]],
+    ["The pickup address is 123 Main St, Anytown, USA. The package is 12\" x 8\" x 6\" and weighs 5 lbs. I'd like it to be picked up tomorrow between 10 AM and 12 PM.", ["Got it! Your pickup has been scheduled for tomorrow between 10 AM and 12 PM at 123 Main St, Anytown, USA."]],
+    ["I need assistance with customs clearance for an international shipment.", ["Sure thing! Could you provide me with the details of the shipment, including the contents, declared value, and destination country?"]],
+    ["It's a shipment of electronics with a declared value of $1000, and it's going to France.", ["Thank you. I'll provide you with the necessary customs documentation and requirements for shipping electronics to France."]],
+    ["What are the shipping options for overnight delivery?", ["We offer several overnight shipping options, including express air, priority ground, and same-day delivery in select areas. Can you provide me with the origin and destination zip codes, as well as the package dimensions and weight?"]],
+    ["The package is shipping from 987 Elm St, Cityville, to 456 Oak St, Townsville. It's 10\" x 6\" x 4\" and weighs 2 lbs.", ["Thank you. Based on the information provided, I recommend our express air option for guaranteed overnight delivery. Would you like to proceed with that?"]],
+    ["I need to change the delivery address for my package.", ["Sure thing! Please provide me with the tracking number and the new delivery address."]],
+    ["The tracking number is ABD34Y6J, and the new address is 789 Maple Ave, Villagetown.", ["Thanks for providing the information. I've updated the delivery address for tracking number ABD34Y6J to 789 Maple Ave, Villagetown."]],
 
-    ["touring bikes", ["What do you prefer? We have KTM 390, Royal Enfield Himalayan."]],
-    ["ktm 390 duke", ["OK, check the link of the product https://www.zigwheels.com/ktm-bikes/duke-390"]],
-    ["Royal Enfield Himalayan", ["OK, check the link of the product https://www.royalenfield.com/in/en/motorcycles/new-himalayan/"]],
-    ["road bikes", ["What do you prefer? We have Polygon Bend R2,Rockfire ridge 29"]],
-    ["Polygon Bend R2", ["OK, check the link of the product https://www.choosemybicycle.com/en/bicycles/polygon-bend-r2-2022"]],
-    ["Rockfire ridge 29", ["OK, check the link of the product https://www.choosemybicycle.com/en/bicycles/rockfire-ridge-29"]],
-    ["mountain bikes bikes", ["What do you prefer? We have Hercules CX70 18S 26T Black/Fresh Green,Montra Chord Medium Ruby Red"]],
-    ["Hercules CX70 18S 26T Black/Fresh Green", ["OK, check the link of the product https://www.trackandtrail.in/cycles/hercules-mtb-single-speed/hercules-topgear-cx70-18s?id=2606"]],
-    ["Montra Chord Medium Ruby Red", ["OK, check the link of the product https://www.trackandtrail.in/cycles/offers/montra-chord-offers?id=3834"]],
-
-    ["mobile phones", ["What do you prefer? We have iphone,samsung"]],
-    ["iphone", ["OK, check the link of the product https://www.apple.com/in/iphone-15-pro/"]],
-    ["samsung", ["OK, check the link of the product https://www.samsung.com/in/"]],
-    ["laptops", ["What do you prefer? We have Lenovo,HP"]],
-    ["Lenovo", ["OK, check the link of the product https://www.lenovo.com/in/en/"]],
-    ["HP", ["OK, check the link of the product https://www.hp.com/in-en/shop/laptops-tablets.html"]],
-    ["washing machine", ["What do you prefer? We have LG,Samsung"]],
-    ["LG", ["OK, check the link of the product https://www.lg.com/in/refrigerators"]],
-    ["Samsung", ["OK, check the link of the product https://www.samsung.com/in/refrigerators/all-refrigerators/"]],
-
-
-    ["kids fashion", ["What do you prefer? We have T-Shirts, Jeans"]],
-    ["T-Shirts", ["OK, check the link of the product https://www.myntra.com/kids-tshirts"]],
-    ["Jeans", ["OK, check the link of the product https://www.myntra.com/kids-jeans"]],
-    ["womens fashion", ["What do you prefer? We have Western Wear , Ethnic wear"]],
-    ["Western Wear", ["OK, check the link of the product https://www.myntra.com/womens-western-wear"]],
-    ["Ethnic Wear", ["OK, check the link of the product https://www.myntra.com/womens-ethnic%20wear"]],
-    ["mens fashion", ["What do you prefer? We have Shirt,Jeans"]],
-    ["Shirt", ["OK, check the link of the product https://www.ajio.com/men-shirts/c/830216013"]],
-    ["Jeans", ["OK, check the link of the product https://www.myntra.com/men-jeans"]],
-
-    ["negotiate", ["Do you wish to negotiate?"]],
-    #["do you wish to negotiate the discount percentage", ["Yes", "No"]],
-    ["price", ["Please check the price on website link."]],
-    ["yes", ["Ok Great lets Begin then"]],
-    ["no", ["Ohh!That's Fine"]],
-    ["show discount", ["If you are lucky, you can get a discount of up to 10-20 percent."]],
-    ["available discount", ["If you are lucky, you can get a discount of up to 10-20 percent"]],
-    ["discount", ["If you are lucky, you can get a discount of up to 10-20 percent"]],
-    ["how much discount", ["If you are lucky, you can get a discount of up to 10-20 percent.How much Discount do you want?"]],
-    ["20%", ["That is too much to ask! How about a discount of 10 percent (accept|reject)"]],
-    ["accept", ["Tell me your discount percentage?(Just Enter Value)"]],
-    ["reject", ["You can continue to negotiate or choose a different product."]],
-
-    ["discount of 18 ", ["Negotiating... How about a discount of 11 percent (Y/N)"]],
-    ["discount of 16 ", ["Negotiating... How about a discount of 12 percent(Y/N)"]],
-    ["discount of 15 ", ["Negotiating... How about a discount of 12.5 percent (Y/N)"]],
-    ["discount of 14 ", ["Negotiating... How about a discount of 13 percent (Y/N)"]],
-
-    ["18", ["Negotiating... How about a discount of 11 percent (Y/N"]],
-    
-    ["16", ["Negotiating... How about a discount of 12 percent (Y/N)"]],
-    ["15", ["Negotiating... How about a discount of 12.5 percent (Y/N)"]],
-    ["14", ["Negotiating... How about a discount of 13 percent (Y/N)"]],
-
-    ["how about a discount of 18", ["Negotiating... How about a discount of 11 percent (Y/N)"]],
-    ["how about a discount of 16", ["Negotiating... How about a discount of 12 percent (Y/N)"]],
-    ["how about a discount of 15", ["Negotiating... How about a discount of 12.5 percent(Y/N)"]],
-    ["how about a discount of 14", ["Negotiating... How about a discount of 13 percent (Y/N)"]],
-    ["Y", ["Ok hurrah the deal is final Discount at 14 percent"]],
-    ["N", ["Feel free to propose another discount percentage."]],
-
-    ["help", ["Sure, I can help you with information about available products, negotiations, and more."]],
-    ["what else can you help with", ["I can assist you with product recommendations, order tracking, and any questions you may have."]],
-    ["bye|goodbye", ["Goodbye! If you have any more questions, feel free to ask.Happy Shopping"]],
-    ["Thank You|Thanks|thnx", ["I am Glad that i was able to help you,Happy Shopping"]],
+    ["help", ["Sure, How can i help you ? "]],
+    ["bye|goodbye", ["Goodbye! If you have any more questions, feel free to ask."]],
+    ["Thank You|Thanks|thnx", ["I am Glad that i was able to help you,Have a nice day !!"]],
 ]
 
 extended_price_negotiation_chatbot = Chat(extended_price_negotiation_pairs, reflections)
+
 def chatbot_response(user_input):
     response = extended_price_negotiation_chatbot.respond(user_input)
     if response:
@@ -95,21 +39,16 @@ def chatbot_response(user_input):
 # Flask routes
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # Since there's no need for index.html, redirect to chat.html
+    return render_template("chat.html")
 
 @app.route("/chat", methods=["POST"])
 def chat():
     user_input = request.form.get("user_input")
     
     # Your chatbot logic goes here
-    #bot_response = extended_price_negotiation_chatbot.respond(user_input)
-    bot_response=chatbot_response(user_input)
+    bot_response = chatbot_response(user_input)
     return render_template("chat.html", user_input=user_input, bot_response=bot_response)
-
-@app.route("/cart")
-def cart():
-    return render_template("cart.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
